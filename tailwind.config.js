1d519c8db5c70af94d0226afd4e04407fb90d09b
dylan_huang <wongwc1998@gmail.com>
2023-09-03 01:17:35 +0800

    add cfg files

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage:{
+        'unsplash': 'url(./public/unsplash.jpg)'
+      }},
+      fontFamily: {
+        'norse': ['Norse-Bold']
+      }
+  },
+  plugins: [],
+}
